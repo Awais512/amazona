ff7f619d1c1f8f85fed5677bb3fac6fed79221c4
awais512 <raza.awais25@gmail.com>
2021-11-23 16:32:17 +0500

    Login into Application

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout/Layout';
 import NextLink from 'next/link';
 import {
@@ -10,12 +10,28 @@ import {
   Link,
 } from '@material-ui/core';
 import useStyles from '../utils/style';
+import axios from 'axios';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const classes = useStyles();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = axios.post(`/api/users/login`, { email, password });
+      // setEmail(data);
+      // console.log(data);
+      alert('Login Success');
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message);
+      console.log(err.message);
+    }
+  };
   return (
     <Layout title='Login'>
-      <form className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component='h1' variant='h1'>
           Login
         </Typography>
@@ -27,6 +43,7 @@ const Login = () => {
               id='email'
               label='Email'
               inputProps={{ type: 'email' }}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </ListItem>
           <ListItem>
@@ -36,6 +53,7 @@ const Login = () => {
               id='password'
               label='Password'
               inputProps={{ type: 'password' }}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </ListItem>
           <ListItem>
